a70eb5a31fa43fa18b1c17061b460205b02e6137
CristinaSilvaEng <cristina.silva@openmailbox.org>
2015-09-22 21:02:44 -0300

    Classes Modelos
    Employee, Item, Sale, Product

---
+++ MarotoPDV/ItemModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarotoPDV
+{
+    class ItemModel
+    {
+        public int ItemID { get; set; }
+        public ProductModel Product { get; set; }
+        public int Quantity { get; set; }
+        public double Parcial { get; set; }
+    }
+}
---
+++ MarotoPDV/SaleModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarotoPDV
+{
+    class SaleModel
+    {
+        public SaleModel()
+        {
+            Items = new HashSet<ItemModel>();
+        }
+
+        public int SaleID { get; set; }
+        public EmployeeModel Employee { get; set; }
+        public double Total { get; set; }
+        public DateTime Timestamp { get; set; }
+        public ICollection<ItemModel> Items { get; set; }
+    }
+}
