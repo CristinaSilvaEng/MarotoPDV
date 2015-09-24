666695a03b79c5387177c05f31b6b9a85130f7a5
CristinaSilvaEng <cristina.silva@openmailbox.org>
2015-09-24 20:04:34 -0300

    Tela de Vendas
    Busca por items, adicionar ao carrinho.
    
    Em XP com @LFelipeMDeO

--- MarotoPDV/SaleForm.Designer.cs
+++ MarotoPDV/SaleForm.Designer.cs
@@ -30,35 +30,37 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SaleForm));
             this.groupBox_sale = new System.Windows.Forms.GroupBox();
-            this.listView_saleItems = new System.Windows.Forms.ListView();
+            this.listBox_saleItems = new System.Windows.Forms.ListBox();
+            this.groupBox_search = new System.Windows.Forms.GroupBox();
+            this.listBox_products = new System.Windows.Forms.ListBox();
+            this.label_quantity = new System.Windows.Forms.Label();
+            this.numericUpDown_quantity = new System.Windows.Forms.NumericUpDown();
+            this.button_cart = new System.Windows.Forms.Button();
+            this.button_clean = new System.Windows.Forms.Button();
+            this.button_searchID = new System.Windows.Forms.Button();
+            this.textBox_id = new System.Windows.Forms.TextBox();
+            this.label_id = new System.Windows.Forms.Label();
+            this.groupBox_total = new System.Windows.Forms.GroupBox();
+            this.label_value = new System.Windows.Forms.Label();
+            this.label_currency = new System.Windows.Forms.Label();
             this.button_generate = new System.Windows.Forms.Button();
             this.button_logout = new System.Windows.Forms.Button();
             this.label_idOperator = new System.Windows.Forms.Label();
             this.label_operator = new System.Windows.Forms.Label();
-            this.groupBox_total = new System.Windows.Forms.GroupBox();
-            this.label_currency = new System.Windows.Forms.Label();
-            this.label_value = new System.Windows.Forms.Label();
-            this.groupBox_search = new System.Windows.Forms.GroupBox();
-            this.label_id = new System.Windows.Forms.Label();
-            this.textBox_id = new System.Windows.Forms.TextBox();
-            this.button_searchID = new System.Windows.Forms.Button();
             this.button_cancel = new System.Windows.Forms.Button();
-            this.listView_products = new System.Windows.Forms.ListView();
-            this.button_clean = new System.Windows.Forms.Button();
-            this.button_cart = new System.Windows.Forms.Button();
-            this.numericUpDown_quantity = new System.Windows.Forms.NumericUpDown();
-            this.label_quantity = new System.Windows.Forms.Label();
+            this.marotoDBDataSet = new MarotoPDV.MarotoDBDataSet();
             this.groupBox_sale.SuspendLayout();
-            this.groupBox_total.SuspendLayout();
             this.groupBox_search.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_quantity)).BeginInit();
+            this.groupBox_total.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.marotoDBDataSet)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox_sale
             // 
+            this.groupBox_sale.Controls.Add(this.listBox_saleItems);
             this.groupBox_sale.Controls.Add(this.groupBox_search);
             this.groupBox_sale.Controls.Add(this.groupBox_total);
-            this.groupBox_sale.Controls.Add(this.listView_saleItems);
             this.groupBox_sale.Location = new System.Drawing.Point(12, 12);
             this.groupBox_sale.Name = "groupBox_sale";
             this.groupBox_sale.Size = new System.Drawing.Size(613, 412);
@@ -66,13 +68,131 @@
             this.groupBox_sale.TabStop = false;
             this.groupBox_sale.Text = "Sale";
             // 
-            // listView_saleItems
+            // listBox_saleItems
             // 
-            this.listView_saleItems.Location = new System.Drawing.Point(6, 19);
-            this.listView_saleItems.Name = "listView_saleItems";
-            this.listView_saleItems.Size = new System.Drawing.Size(317, 387);
-            this.listView_saleItems.TabIndex = 0;
-            this.listView_saleItems.UseCompatibleStateImageBehavior = false;
+            this.listBox_saleItems.FormattingEnabled = true;
+            this.listBox_saleItems.Location = new System.Drawing.Point(6, 19);
+            this.listBox_saleItems.Name = "listBox_saleItems";
+            this.listBox_saleItems.Size = new System.Drawing.Size(320, 381);
+            this.listBox_saleItems.TabIndex = 3;
+            // 
+            // groupBox_search
+            // 
+            this.groupBox_search.Controls.Add(this.listBox_products);
+            this.groupBox_search.Controls.Add(this.label_quantity);
+            this.groupBox_search.Controls.Add(this.numericUpDown_quantity);
+            this.groupBox_search.Controls.Add(this.button_cart);
+            this.groupBox_search.Controls.Add(this.button_clean);
+            this.groupBox_search.Controls.Add(this.button_searchID);
+            this.groupBox_search.Controls.Add(this.textBox_id);
+            this.groupBox_search.Controls.Add(this.label_id);
+            this.groupBox_search.Location = new System.Drawing.Point(332, 19);
+            this.groupBox_search.Name = "groupBox_search";
+            this.groupBox_search.Size = new System.Drawing.Size(275, 316);
+            this.groupBox_search.TabIndex = 2;
+            this.groupBox_search.TabStop = false;
+            this.groupBox_search.Text = "Search";
+            // 
+            // listBox_products
+            // 
+            this.listBox_products.FormattingEnabled = true;
+            this.listBox_products.Location = new System.Drawing.Point(9, 51);
+            this.listBox_products.Name = "listBox_products";
+            this.listBox_products.Size = new System.Drawing.Size(260, 225);
+            this.listBox_products.TabIndex = 17;
+            // 
+            // label_quantity
+            // 
+            this.label_quantity.AutoSize = true;
+            this.label_quantity.Location = new System.Drawing.Point(3, 286);
+            this.label_quantity.Name = "label_quantity";
+            this.label_quantity.Size = new System.Drawing.Size(49, 13);
+            this.label_quantity.TabIndex = 20;
+            this.label_quantity.Text = "Quantity:";
+            // 
+            // numericUpDown_quantity
+            // 
+            this.numericUpDown_quantity.Location = new System.Drawing.Point(58, 284);
+            this.numericUpDown_quantity.Name = "numericUpDown_quantity";
+            this.numericUpDown_quantity.Size = new System.Drawing.Size(49, 20);
+            this.numericUpDown_quantity.TabIndex = 19;
+            // 
+            // button_cart
+            // 
+            this.button_cart.Location = new System.Drawing.Point(113, 281);
+            this.button_cart.Name = "button_cart";
+            this.button_cart.Size = new System.Drawing.Size(75, 23);
+            this.button_cart.TabIndex = 18;
+            this.button_cart.Text = "Add to Cart";
+            this.button_cart.UseVisualStyleBackColor = true;
+            this.button_cart.Click += new System.EventHandler(this.button_cart_Click);
+            // 
+            // button_clean
+            // 
+            this.button_clean.Location = new System.Drawing.Point(194, 281);
+            this.button_clean.Name = "button_clean";
+            this.button_clean.Size = new System.Drawing.Size(75, 23);
+            this.button_clean.TabIndex = 17;
+            this.button_clean.Text = "Clean";
+            this.button_clean.UseVisualStyleBackColor = true;
+            this.button_clean.Click += new System.EventHandler(this.button_clean_Click);
+            // 
+            // button_searchID
+            // 
+            this.button_searchID.Location = new System.Drawing.Point(194, 22);
+            this.button_searchID.Name = "button_searchID";
+            this.button_searchID.Size = new System.Drawing.Size(75, 23);
+            this.button_searchID.TabIndex = 6;
+            this.button_searchID.Text = "Search";
+            this.button_searchID.UseVisualStyleBackColor = true;
+            this.button_searchID.Click += new System.EventHandler(this.button_searchID_Click);
+            // 
+            // textBox_id
+            // 
+            this.textBox_id.Location = new System.Drawing.Point(58, 25);
+            this.textBox_id.Name = "textBox_id";
+            this.textBox_id.Size = new System.Drawing.Size(130, 20);
+            this.textBox_id.TabIndex = 1;
+            // 
+            // label_id
+            // 
+            this.label_id.AutoSize = true;
+            this.label_id.Location = new System.Drawing.Point(6, 28);
+            this.label_id.Name = "label_id";
+            this.label_id.Size = new System.Drawing.Size(21, 13);
+            this.label_id.TabIndex = 0;
+            this.label_id.Text = "ID:";
+            // 
+            // groupBox_total
+            // 
+            this.groupBox_total.Controls.Add(this.label_value);
+            this.groupBox_total.Controls.Add(this.label_currency);
+            this.groupBox_total.Location = new System.Drawing.Point(332, 341);
+            this.groupBox_total.Name = "groupBox_total";
+            this.groupBox_total.Size = new System.Drawing.Size(275, 65);
+            this.groupBox_total.TabIndex = 1;
+            this.groupBox_total.TabStop = false;
+            this.groupBox_total.Text = "Total";
+            // 
+            // label_value
+            // 
+            this.label_value.AutoSize = true;
+            this.label_value.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_value.Location = new System.Drawing.Point(191, 25);
+            this.label_value.Name = "label_value";
+            this.label_value.Size = new System.Drawing.Size(78, 26);
+            this.label_value.TabIndex = 1;
+            this.label_value.Text = "297,00";
+            // 
+            // label_currency
+            // 
+            this.label_currency.AutoSize = true;
+            this.label_currency.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_currency.Location = new System.Drawing.Point(160, 25);
+            this.label_currency.Name = "label_currency";
+            this.label_currency.Size = new System.Drawing.Size(24, 26);
+            this.label_currency.TabIndex = 0;
+            this.label_currency.Text = "$";
             // 
             // button_generate
             // 
@@ -111,80 +231,6 @@
             this.label_operator.TabIndex = 5;
             this.label_operator.Text = "OP #";
             // 
-            // groupBox_total
-            // 
-            this.groupBox_total.Controls.Add(this.label_value);
-            this.groupBox_total.Controls.Add(this.label_currency);
-            this.groupBox_total.Location = new System.Drawing.Point(332, 341);
-            this.groupBox_total.Name = "groupBox_total";
-            this.groupBox_total.Size = new System.Drawing.Size(275, 65);
-            this.groupBox_total.TabIndex = 1;
-            this.groupBox_total.TabStop = false;
-            this.groupBox_total.Text = "Total";
-            // 
-            // label_currency
-            // 
-            this.label_currency.AutoSize = true;
-            this.label_currency.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_currency.Location = new System.Drawing.Point(160, 25);
-            this.label_currency.Name = "label_currency";
-            this.label_currency.Size = new System.Drawing.Size(24, 26);
-            this.label_currency.TabIndex = 0;
-            this.label_currency.Text = "$";
-            // 
-            // label_value
-            // 
-            this.label_value.AutoSize = true;
-            this.label_value.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_value.Location = new System.Drawing.Point(191, 25);
-            this.label_value.Name = "label_value";
-            this.label_value.Size = new System.Drawing.Size(78, 26);
-            this.label_value.TabIndex = 1;
-            this.label_value.Text = "297,00";
-            // 
-            // groupBox_search
-            // 
-            this.groupBox_search.Controls.Add(this.label_quantity);
-            this.groupBox_search.Controls.Add(this.numericUpDown_quantity);
-            this.groupBox_search.Controls.Add(this.button_cart);
-            this.groupBox_search.Controls.Add(this.button_clean);
-            this.groupBox_search.Controls.Add(this.listView_products);
-            this.groupBox_search.Controls.Add(this.button_searchID);
-            this.groupBox_search.Controls.Add(this.textBox_id);
-            this.groupBox_search.Controls.Add(this.label_id);
-            this.groupBox_search.Location = new System.Drawing.Point(332, 19);
-            this.groupBox_search.Name = "groupBox_search";
-            this.groupBox_search.Size = new System.Drawing.Size(275, 316);
-            this.groupBox_search.TabIndex = 2;
-            this.groupBox_search.TabStop = false;
-            this.groupBox_search.Text = "Search";
-            // 
-            // label_id
-            // 
-            this.label_id.AutoSize = true;
-            this.label_id.Location = new System.Drawing.Point(6, 28);
-            this.label_id.Name = "label_id";
-            this.label_id.Size = new System.Drawing.Size(21, 13);
-            this.label_id.TabIndex = 0;
-            this.label_id.Text = "ID:";
-            // 
-            // textBox_id
-            // 
-            this.textBox_id.Location = new System.Drawing.Point(58, 25);
-            this.textBox_id.Name = "textBox_id";
-            this.textBox_id.Size = new System.Drawing.Size(130, 20);
-            this.textBox_id.TabIndex = 1;
-            // 
-            // button_searchID
-            // 
-            this.button_searchID.Location = new System.Drawing.Point(194, 22);
-            this.button_searchID.Name = "button_searchID";
-            this.button_searchID.Size = new System.Drawing.Size(75, 23);
-            this.button_searchID.TabIndex = 6;
-            this.button_searchID.Text = "Search";
-            this.button_searchID.UseVisualStyleBackColor = true;
-            this.button_searchID.Click += new System.EventHandler(this.button_searchID_Click);
-            // 
             // button_cancel
             // 
             this.button_cancel.Location = new System.Drawing.Point(149, 440);
@@ -194,48 +240,10 @@
             this.button_cancel.Text = "Cancel Sale";
             this.button_cancel.UseVisualStyleBackColor = true;
             // 
-            // listView_products
+            // marotoDBDataSet
             // 
-            this.listView_products.FullRowSelect = true;
-            this.listView_products.Location = new System.Drawing.Point(6, 51);
-            this.listView_products.Name = "listView_products";
-            this.listView_products.Size = new System.Drawing.Size(263, 224);
-            this.listView_products.TabIndex = 16;
-            this.listView_products.UseCompatibleStateImageBehavior = false;
-            // 
-            // button_clean
-            // 
-            this.button_clean.Location = new System.Drawing.Point(194, 281);
-            this.button_clean.Name = "button_clean";
-            this.button_clean.Size = new System.Drawing.Size(75, 23);
-            this.button_clean.TabIndex = 17;
-            this.button_clean.Text = "Clean";
-            this.button_clean.UseVisualStyleBackColor = true;
-            // 
-            // button_cart
-            // 
-            this.button_cart.Location = new System.Drawing.Point(113, 281);
-            this.button_cart.Name = "button_cart";
-            this.button_cart.Size = new System.Drawing.Size(75, 23);
-            this.button_cart.TabIndex = 18;
-            this.button_cart.Text = "Add to Cart";
-            this.button_cart.UseVisualStyleBackColor = true;
-            // 
-            // numericUpDown_quantity
-            // 
-            this.numericUpDown_quantity.Location = new System.Drawing.Point(58, 284);
-            this.numericUpDown_quantity.Name = "numericUpDown_quantity";
-            this.numericUpDown_quantity.Size = new System.Drawing.Size(49, 20);
-            this.numericUpDown_quantity.TabIndex = 19;
-            // 
-            // label_quantity
-            // 
-            this.label_quantity.AutoSize = true;
-            this.label_quantity.Location = new System.Drawing.Point(3, 286);
-            this.label_quantity.Name = "label_quantity";
-            this.label_quantity.Size = new System.Drawing.Size(49, 13);
-            this.label_quantity.TabIndex = 20;
-            this.label_quantity.Text = "Quantity:";
+            this.marotoDBDataSet.DataSetName = "MarotoDBDataSet";
+            this.marotoDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // SaleForm
             // 
@@ -255,11 +263,12 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MarotoPVD";
             this.groupBox_sale.ResumeLayout(false);
-            this.groupBox_total.ResumeLayout(false);
-            this.groupBox_total.PerformLayout();
             this.groupBox_search.ResumeLayout(false);
             this.groupBox_search.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_quantity)).EndInit();
+            this.groupBox_total.ResumeLayout(false);
+            this.groupBox_total.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.marotoDBDataSet)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -275,16 +284,17 @@
         private System.Windows.Forms.GroupBox groupBox_total;
         private System.Windows.Forms.Label label_value;
         private System.Windows.Forms.Label label_currency;
-        private System.Windows.Forms.ListView listView_saleItems;
         private System.Windows.Forms.Button button_generate;
         private System.Windows.Forms.Button button_logout;
         private System.Windows.Forms.Label label_idOperator;
         private System.Windows.Forms.Label label_operator;
         private System.Windows.Forms.Button button_cancel;
-        private System.Windows.Forms.ListView listView_products;
         private System.Windows.Forms.Label label_quantity;
         private System.Windows.Forms.NumericUpDown numericUpDown_quantity;
         private System.Windows.Forms.Button button_cart;
         private System.Windows.Forms.Button button_clean;
+        private System.Windows.Forms.ListBox listBox_products;
+        private System.Windows.Forms.ListBox listBox_saleItems;
+        private MarotoDBDataSet marotoDBDataSet;
     }
 }
--- MarotoPDV/SaleForm.cs
+++ MarotoPDV/SaleForm.cs
@@ -25,6 +25,7 @@ namespace MarotoPDV
 
         private void button_searchID_Click(object sender, EventArgs e)
         {
+            listBox_products.Items.Clear();
             using (SqlConnection sqlConn =
                    new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\MarotoDB.mdf;Integrated Security=True;"))
             {
@@ -39,8 +40,8 @@ namespace MarotoPDV
 
                     if (dataReader.Read())
                     {
-                        listView_products.Items.Add(String.Format("{0,-40}  {1,-12}  {2,3}",
-                            dataReader["NAME"].ToString(), dataReader["PROVIDER"].ToString(), dataReader["QUANTITY"].ToString()));
+                        listBox_products.Items.Add(String.Format("{0,-32}  {1,12}  {2,3}", dataReader["NAME"].ToString().Trim(),
+                            dataReader["PROVIDER"].ToString().Trim(), dataReader["QUANTITY"].ToString().Trim()));
                     }
                     sqlConn.Close();
                 }
@@ -57,6 +58,7 @@ namespace MarotoPDV
 
         private void getAllProducts()
         {
+            listBox_products.Items.Clear();
             using (SqlConnection sqlConn =
                   new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\MarotoDB.mdf;Integrated Security=True;"))
             {
@@ -70,12 +72,46 @@ namespace MarotoPDV
                     Console.WriteLine(dataReader);
                     while (dataReader.Read())
                     {
-                        listView_products.Items.Add(dataReader["NAME"].ToString());
+                        listBox_products.Items.Add(String.Format("{0,-32}  {1,12}  {2,3}", dataReader["NAME"].ToString().Trim(), 
+                            dataReader["PROVIDER"].ToString().Trim(), dataReader["QUANTITY"].ToString().Trim()));
                     }
                     sqlConn.Close();
                 }
             }
         }
 
+        private void button_clean_Click(object sender, EventArgs e)
+        {
+            textBox_id.ResetText();
+            getAllProducts();
+        }
+
+        private void button_cart_Click(object sender, EventArgs e)
+        {
+            if (numericUpDown_quantity.Value > 0 && listBox_products.Items.Count == 1)
+            {
+                using (SqlConnection sqlConn =
+                   new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\MarotoDB.mdf;Integrated Security=True;"))
+                {
+                    using (SqlCommand sqlCommand = new SqlCommand())
+                    {
+                        sqlCommand.Parameters.AddWithValue("id", textBox_id.Text);
+                        sqlCommand.CommandText = "SELECT * FROM PRODUCT WHERE ID = @id";
+                        sqlCommand.Connection = sqlConn;
+                        sqlConn.Open();
+                        SqlDataReader dataReader;
+                        dataReader = sqlCommand.ExecuteReader();
+
+                        if (dataReader.Read())
+                        {
+                            listBox_saleItems.Items.Add(String.Format("{0,-32}  {1,3}", dataReader["NAME"].ToString().Trim()
+                                , numericUpDown_quantity.Value.ToString()));
+                        }
+                        sqlConn.Close();
+                    }
+                }
+            } 
+
+        }
     }
 }
