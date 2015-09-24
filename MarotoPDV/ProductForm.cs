0fb6683e97fdec763666800efe8082ca534bffd3
Luiz Felipe Marques de Oliveira <lfelipemdo@gmail.com>
2015-09-24 15:13:35 -0300

    Complemento do CRUD de produto

--- MarotoPDV/ProductForm.cs
+++ MarotoPDV/ProductForm.cs
@@ -32,7 +32,30 @@ namespace MarotoPDV
 
         private void button_create_Click(object sender, EventArgs e)
         {
-            
+            using (SqlConnection sqlConn =
+                   new SqlConnection(connectionString))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand())
+                {
+                    sqlCommand.CommandText = "INSERT INTO PRODUCT VALUES (@id, @name, @provider, @expiration_date, @quantity, @purchase_price, @sale_price, @description)";
+
+                    sqlCommand.Parameters.AddWithValue("id", textBox_id.Text);
+                    sqlCommand.Parameters.AddWithValue("name", textBox_name.Text);
+                    sqlCommand.Parameters.AddWithValue("provider", textBox_fornecedor.Text);
+                    sqlCommand.Parameters.AddWithValue("expiration_date", dateTimePicker_data_validade.Value);
+                    sqlCommand.Parameters.AddWithValue("quantity", Int32.Parse(textBox_qtd_estoque.Text));
+                    sqlCommand.Parameters.AddWithValue("purchase_price", Decimal.Parse(textBox_preco_compra.Text));
+                    sqlCommand.Parameters.AddWithValue("sale_price", Decimal.Parse(textBox_preco_venda.Text));
+                    sqlCommand.Parameters.AddWithValue("description", textBox_descricao.Text);
+
+                    sqlCommand.Connection = sqlConn;
+                    sqlConn.Open();
+
+                    sqlCommand.ExecuteNonQuery();
+
+                    sqlConn.Close();
+                }
+            }
         }
 
         private void button_search_Click(object sender, EventArgs e)
@@ -54,13 +77,13 @@ namespace MarotoPDV
                     if (dataReader.Read())
                     {
                         product = new ProductModel(Int32.Parse(dataReader["ID"].ToString()),
-                                            dataReader[1].ToString(),
-                                            dataReader[2].ToString(),
-                                            Convert.ToDateTime(dataReader["EXPIRATION_DATE"]),
+                                            dataReader["NAME"].ToString(),
+                                            dataReader["PROVIDER"].ToString(),
+                                            Convert.ToDateTime(dataReader["EXPIRATION_DATE"].ToString()),
                                             Int32.Parse(dataReader["QUANTITY"].ToString()),
-                                            Decimal.Parse(dataReader[4].ToString()),
-                                            Decimal.Parse(dataReader[5].ToString()),
-                                            dataReader[6].ToString());
+                                            Decimal.Parse(dataReader["PURCHASE_PRICE"].ToString()),
+                                            Decimal.Parse(dataReader["SALE_PRICE"].ToString()),
+                                            dataReader["DESCRIPTION"].ToString());
                     }
                     sqlConn.Close();
 
@@ -86,12 +109,51 @@ namespace MarotoPDV
 
         private void button_update_Click(object sender, EventArgs e)
         {
+            using (SqlConnection sqlConn =
+                   new SqlConnection(connectionString))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand())
+                {
+                    sqlCommand.CommandText = "UPDATE PRODUCT SET NAME = @name, PROVIDER = @provider, EXPIRATION_DATE = @expiration_date, " +
+                                                    "QUANTITY = @quantity, PURCHASE_PRICE = @purchase_price, SALE_PRICE = @sale_price, DESCRIPTION = @description WHERE ID = @id";
 
+                    sqlCommand.Parameters.AddWithValue("id", textBox_id.Text);
+                    sqlCommand.Parameters.AddWithValue("name", textBox_name.Text);
+                    sqlCommand.Parameters.AddWithValue("provider", textBox_fornecedor.Text);
+                    sqlCommand.Parameters.AddWithValue("expiration_date", dateTimePicker_data_validade.Value);
+                    sqlCommand.Parameters.AddWithValue("quantity", Int32.Parse(textBox_qtd_estoque.Text));
+                    sqlCommand.Parameters.AddWithValue("purchase_price", Decimal.Parse(textBox_preco_compra.Text));
+                    sqlCommand.Parameters.AddWithValue("sale_price", Decimal.Parse(textBox_preco_venda.Text));
+                    sqlCommand.Parameters.AddWithValue("description", textBox_descricao.Text);
+
+                    sqlCommand.Connection = sqlConn;
+                    sqlConn.Open();
+
+                    sqlCommand.ExecuteNonQuery();
+
+                    sqlConn.Close();
+                }
+            }
         }
 
         private void button_delete_Click(object sender, EventArgs e)
         {
+            using (SqlConnection sqlConn =
+                   new SqlConnection(connectionString))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand())
+                {
+                    sqlCommand.CommandText = "DELETE FROM PRODUCT WHERE ID = @id";
 
+                    sqlCommand.Parameters.AddWithValue("id", textBox_id.Text);
+                    sqlCommand.Connection = sqlConn;
+                    sqlConn.Open();
+
+                    sqlCommand.ExecuteNonQuery();
+
+                    sqlConn.Close();
+                }
+            }
         }
     }
 }
