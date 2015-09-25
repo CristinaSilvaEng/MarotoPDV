97be798a0a67041b5a77d3b2e61ecd481c464c07
CristinaSilvaEng <cristina.silva@openmailbox.org>
2015-09-24 22:33:46 -0300

    Altera incone de Form.

--- MarotoPDV/ProductForm.Designer.cs
+++ MarotoPDV/ProductForm.Designer.cs
@@ -307,6 +307,7 @@
             this.Controls.Add(this.button_update);
             this.Controls.Add(this.button_create);
             this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ProductForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MarotoPDV";
